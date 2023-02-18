c5eeefaf03b1a7ed949f9ff9f42d8c0c1dbb5c1f
Bryan <bryan.romero@gft.com>
2023-02-18 17:56:27 +0100

    Adding trainers section

--- js/script.js
+++ js/script.js
@@ -65,3 +65,25 @@ var swiper = new Swiper(".feature-slider", {
   speed: 10000,
   freeModeMomentum: false,
 });
+
+var swiper = new Swiper(".trainer-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
