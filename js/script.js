a09dcff5d159fb4db14b78a94d4f1f6607b2af47
Bryan <bryan.romero@gft.com>
2023-02-22 17:06:02 +0100

    Responsive burger menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-// Get the selected language element and menu item
 let navbar = document.querySelector(".navbar");
+let menu = document.getElementById("menu-btn");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
 window.onscroll = () => {
-  navbar.classList.remove("active");
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
 
   if (window.scrollY > 0) {
     document.querySelector(".header").classList.add("active");
